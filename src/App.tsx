55b19806b24b15634eaa596c15a39d89e54aef59
phoebele212 <phoebe.le2121@gmail.com>
2025-07-24 14:10:53 -0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+            <h1>🐱 StudyCar</h1>
+            <p>Welcome! Let's start studying!</p>
+        </div>
+    );
+}
+
+export default App;
